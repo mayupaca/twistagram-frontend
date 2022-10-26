8f7f17e88a049f74f4658d6a0f31b04a8466b2d2
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-10-26 19:49:24 -0400

    created and styled rightbar component

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
-import './Rightbar.css'
+import React from "react";
+import "./Rightbar.css";
 
 export default function Rightbar() {
   return (
-    <div className='rightbar'>Rightbar</div>
-  )
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="eventContainer">
+          <img src="assets/star.png" alt="" className="starImg" />
+          <span className="eventText">
+            <b>Follower limited event!!</b>
+          </span>
+        </div>
+        <img src="assets/event.jpeg" alt="" className="eventImg" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/1.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">James code</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/2.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">James code</span>
+          </li>
+        </ul>
+        <p className="promotionTitle">Promotion</p>
+        <img
+          src="assets/promotion/promotion1.jpeg"
+          alt=""
+          className="rightbarPromotionImg"
+        />
+        <p className="promotionName">Shopping</p>
+        <img
+          src="assets/promotion/promotion2.jpeg"
+          alt=""
+          className="rightbarPromotionImg"
+        />
+        <p className="promotionName">Car company</p>
+        <img
+          src="assets/promotion/promotion3.jpeg"
+          alt=""
+          className="rightbarPromotionImg"
+        />
+        <p className="promotionName">James and Co.</p>
+      </div>
+    </div>
+  );
 }
