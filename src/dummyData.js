52b1213500edee8f4cf2be9b9a01a02a53178f47
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-10-27 13:33:24 -0400

    created dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,74 @@
+export const Users = [
+  {
+    id: 1,
+    profilePicture: "assets/person/1.jpeg",
+    username: "Tommy",
+  },
+  {
+    id: 2,
+    profilePicture: "assets/person/2.jpeg",
+    username: "James",
+  },
+  {
+    id: 3,
+    profilePicture: "assets/person/3.jpeg",
+    username: "Jessie",
+  },
+  {
+    id: 4,
+    profilePicture: "assets/person/4.jpeg",
+    username: "Lily",
+  },
+  {
+    id: 5,
+    profilePicture: "assets/person/5.jpeg",
+    username: "Anna",
+  },
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: "I code everyday!!",
+    photo: "assets/post/1.jpeg",
+    date: "5 minutes ago",
+    userId: 1,
+    like: 20,
+    comment: 4,
+  },
+  {
+    id: 2,
+    desc: "MERN stack is awesome!!",
+    photo: "assets/post/2.jpeg",
+    date: "2 minutes ago",
+    userId: 2,
+    like: 140,
+    comment: 13,
+  },
+  {
+    id: 3,
+    desc: "Eat health stay health",
+    photo: "assets/post/3.jpeg",
+    date: "10 minutes ago",
+    userId: 3,
+    like: 230,
+    comment: 29,
+  },
+  {
+    id: 4,
+    photo: "assets/post/4.jpeg",
+    date: "1 minutes ago",
+    userId: 4,
+    like: 10,
+    comment: 1,
+  },
+  {
+    id: 5,
+    desc: "Winter has come...",
+    photo: "assets/post/5.jpeg",
+    date: "1 minutes ago",
+    userId: 5,
+    like: 100,
+    comment: 3,
+  },
+];
