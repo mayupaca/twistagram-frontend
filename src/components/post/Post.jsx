a37a2d99fbd1af0cef199b55809790ac77069b59
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-10-30 12:06:46 -0400

    access api and fetch data

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,7 +1,9 @@
 import { MoreVert } from "@mui/icons-material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Post.css";
-import { Users } from "../../dummyData";
+import axios from "axios";
+
+// import { Users } from "../../dummyData";
 
 export default function Post({ post }) {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
@@ -11,6 +13,22 @@ export default function Post({ post }) {
   // Likeが押されているかどうか確認
   // false押されてない
   const [isLiked, setIsLiked] = useState(false);
+
+  const [user, setUser] = useState({});
+  // apiをたたく
+  useEffect(() => {
+    const fetchUser = async () => {
+      // axiosの.getメソッドを使ってデータベースからデータを取ってくる
+      const response = await axios.get(
+        // 投稿したuserのuserid
+        `/users/${post.userId}`
+      );
+      console.log(response);
+      setUser(response.data);
+    };
+    fetchUser();
+    // 一回だけapiをたたきたいから最後に[]をつける
+  }, []);
 
   const handleLike = () => {
     // likedだったらlikeを一つ減らす
@@ -22,16 +40,9 @@ export default function Post({ post }) {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img
-              src={
-                PUBLIC_FOLDER +
-                Users.filter((user) => user.id === post.id)[0].profilePicture
-              }
-              alt=""
-              className="postProfileImg"
-            />
+            <img src={user.profilePicture} alt="" className="postProfileImg" />
             <span className="postUsername">
-              {Users.filter((user) => user.id === post.id)[0].username}
+              {user.username}
             </span>
             <span className="postDate">{post.date}</span>
           </div>
--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./Timeline.css";
-import { Posts } from "../../dummyData";
+import axios from "axios";
+// import { Posts } from "../../dummyData";
 
 export default function Timeline() {
+  const [posts, setPosts] = useState([]);
+  // apiをたたく
+  useEffect(() => {
+    const fetchPosts = async () => {
+      // axiosの.getメソッドを使ってデータベースからデータを取ってくる
+      const response = await axios.get(
+        "/posts/timeline/635697b796721dc5c65e122a"
+      );
+      // console.log(response);
+      setPosts(response.data);
+    };
+    fetchPosts();
+    // 一回だけapiをたたきたいから最後に[]をつける
+  }, []);
+
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        {Posts.map((post) => (
+        {posts.map((post) => (
           <Post post={post} key={post.id} />
         ))}
       </div>
