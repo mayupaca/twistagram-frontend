dfd9d5701f7cbc9e7d8a67807f379f83a69edcbb
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-11-02 08:53:06 -0400

    implement login and registre auth function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
+import { useContext } from "react";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { AuthContext } from "./state/AuthContext";
+
+// useContextを使ってグローバル変数のAuthContextを持ってくる
+// userをどこからでも使えるようにする
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
-<Router>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/profile/:username" element={<Profile />} />
-  </Routes>
-</Router>
-  ) 
-  
+    <Router>
+      <Routes>
+        <Route path="/" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Register.css";
 
 export default function Register() {
+  // useRefを使って、form>inputの値を簡単に取ってくる
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordConfirm = useRef();
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // check password and passwordConfirm
+    if (password.current.value !== passwordConfirm.current.value) {
+      // .setCustomValidity関数
+      passwordConfirm.current.setCustomValidity(
+        "Your passwor is not the same."
+      );
+    } else {
+      try {
+        // 登録するユーザー情報
+        const user = {
+          username: username.current.value,
+          email: email.current.value,
+          password: password.current.value,
+          passwordConfirm: passwordConfirm.current.value,
+        };
+        // registerのapiたたく
+        await axios.post("/auth/register", user)
+        navigate('/login')
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <div className="register">
       <div className="registerWrapper">
@@ -10,23 +46,43 @@ export default function Register() {
           <div className="registerDesc">Let's enjoy!!</div>
         </div>
         <div className="registerRight">
-          <div className="registerBox">
+          <form className="registerBox" onSubmit={(e) => handleSubmit(e)}>
             <p className="registerMsg">Register here!</p>
-            <input type="text" className="registerInput" placeholder="username" />
-            <input type="text" className="registerInput" placeholder="email" />
             <input
               type="text"
+              className="registerInput"
+              placeholder="username"
+              required
+              ref={username}
+            />
+            <input
+              type="email"
+              className="registerInput"
+              placeholder="email"
+              required
+              ref={email}
+            />
+            <input
+              type="password"
               className="registerInput"
               placeholder="password"
+              required
+              minLength="6"
+              ref={password}
             />
             <input
-              type="text"
+              type="password"
               className="registerInput"
               placeholder="confirm password"
+              required
+              minLength="6"
+              ref={passwordConfirm}
             />
-            <button className="registerButton">Sign up</button>
+            <button className="registerButton" type="submit">
+              Sign up
+            </button>
             <button className="registerLoginButton">Login</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
